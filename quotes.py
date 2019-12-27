700c83e9af4de9924447aba90df7289ee21ee6ea
Tuyen Dang <tuyendq@gmail.com>
2019-12-27 13:17:04 +0700

    add meta description

--- quotes.py
+++ quotes.py
@@ -52,14 +52,15 @@ def create_html(file_html, quote_content, quote_author):
 		<head>
 		<meta charset="utf-8">
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-        <link rel="canonical" href="https://www.practicehabits.net/index.html">
+        <meta name="description" content="Practice Habits: Hour of Quote"/>
+        <link rel="canonical" href="https://www.practicehabits.net/">
 		<title>What, Why, and How to practice habit?</title>
 
         <script async src="https://cdn.ampproject.org/v0.js"></script>
 
 		<style amp-custom>
             body {{background-color: #{bg_color}; font-size: 1rem;}}
-            blockquote {{color: #FFFFFF; font-size: 5rem; border-style: solid; padding: 5rem;}}
+            blockquote {{color: #FFFFFF; font-size: 3rem; border-style: solid; padding: 1rem;}}
             .author {{font-size: 2rem; font-style: italic;}}
         </style>
         <style amp-boilerplate>body{{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}}@-webkit-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-moz-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-ms-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@-o-keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}@keyframes -amp-start{{from{{visibility:hidden}}to{{visibility:visible}}}}</style><noscript><style amp-boilerplate>body{{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}}</style></noscript>
